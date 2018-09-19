e7676ba8c2e46f7594112858848cd13f50bdd1b9
informatica <garcell27@gmail.com>
2018-09-19 18:12:20 -0500

    12-09-18

--- src/views/academico/Cursos.vue
+++ src/views/academico/Cursos.vue
@@ -1,21 +1,35 @@
 <template>
     <div class="animated fadeIn">
-      <b-card id="listaTabla">
-          <div slot="header">
+      <b-card id="listaTabla" v-show="accion==0">
+          <div slot="header" v-show="!loading">
             <strong><i class="fa fa-list"></i> LISTA DE CURSOS</strong>
             <div class="card-header-actions">
-              <b-link href="#" class="btn btn-success btn-sm" @click="myModal = true">
+              <b-link href="#" class="btn btn-success btn-sm" @click="crear_curso">
                 <i class="icon-plus"></i> Agregar
               </b-link>
             </div>
           </div>
-        <element-loading active="true" spinner="spinner" color="#FF6700"></element-loading>
+        <element-loading :active="loading" spinner="spinner" color="#FF6700"></element-loading>
         <b-alert :show="viewalertlist" variant="primary" >Actualmente no tienes Ningun Curso registrado</b-alert>
-
       </b-card>
-      <b-card id="FormularioCurso">
-
-        hola mis estimados
+      <b-card id="FormularioCurso" v-show="accion==1||accion==2">
+        <div slot="header" v-show="!loading">
+          <strong><i class="fa fa-list"></i> REGISTRO DE CURSO</strong>
+          <div class="card-header-actions">
+            <b-link href="#" class="btn btn-danger btn-sm" @click="cancelar_accion">
+              <i class="icon-close"></i> Cerrar
+            </b-link>
+          </div>
+        </div>
+        <b-form >
+          <b-form-group
+            label-for="des_curso"
+            description="DESCRIPCION"
+            :label-cols="3"
+            :horizontal="true">
+            <b-form-input v-model="formCurso.descripcion" type="text" placeholder="DESCRIPCION"></b-form-input>
+          </b-form-group>
+        </b-form>
       </b-card>
     </div>
 </template>
@@ -30,6 +44,8 @@
         },
         data:function(){
             return {
+              loading:false,
+              accion:0,
               cursos:[],
               formCurso:{
                 idcurso:null,
@@ -43,27 +59,6 @@
                 formato_id:null,
               },
               cursoView:null,
-              items: [
-                {username: 'Samppa Nori', registered: '2012/01/01', role: 'Member', status: 'Active'},
-                {username: 'Estavan Lykos', registered: '2012/02/01', role: 'Staff', status: 'Banned'},
-                {username: 'Chetan Mohamed', registered: '2012/02/01', role: 'Admin', status: 'Inactive'},
-                {username: 'Derick Maximinus', registered: '2012/03/01', role: 'Member', status: 'Pending'},
-                {username: 'Friderik Dávid', registered: '2012/01/21', role: 'Staff', status: 'Active'},
-                {username: 'Yiorgos Avraamu', registered: '2012/01/01', role: 'Member', status: 'Active'},
-                {username: 'Avram Tarasios', registered: '2012/02/01', role: 'Staff', status: 'Banned'},
-                {username: 'Quintin Ed', registered: '2012/02/01', role: 'Admin', status: 'Inactive'},
-                {username: 'Enéas Kwadwo', registered: '2012/03/01', role: 'Member', status: 'Pending'},
-                {username: 'Agapetus Tadeáš', registered: '2012/01/21', role: 'Staff', status: 'Active'},
-                {username: 'Carwyn Fachtna', registered: '2012/01/01', role: 'Member', status: 'Active'},
-                {username: 'Nehemiah Tatius', registered: '2012/02/01', role: 'Staff', status: 'Banned'},
-                {username: 'Ebbe Gemariah', registered: '2012/02/01', role: 'Admin', status: 'Inactive'},
-                {username: 'Eustorgios Amulius', registered: '2012/03/01', role: 'Member', status: 'Pending'},
-                {username: 'Leopold Gáspár', registered: '2012/01/21', role: 'Staff', status: 'Active'},
-                {username: 'Pompeius René', registered: '2012/01/01', role: 'Member', status: 'Active'},
-                {username: 'Paĉjo Jadon', registered: '2012/02/01', role: 'Staff', status: 'Banned'},
-                {username: 'Micheal Mercurius', registered: '2012/02/01', role: 'Admin', status: 'Inactive'},
-                {username: 'Ganesha Dubhghall', registered: '2012/03/01', role: 'Member', status: 'Pending'}
-              ],
               campos: [
                 {key: 'username'},
                 {key: 'registered'},
@@ -83,13 +78,26 @@
             listar:function(){
               let urlback=this.$store.getters.urlbackend
               let token=localStorage.getItem('token')
+              this.loading=true
               axios.defaults.headers.common['Api-Token'] = token
               axios({url:urlback+'cursos',method:'GET'})
                 .then(response=>{
                   this.cursos=response.data
-                })
+                  this.loading=false
+                }).catch(error=>{
+                  console.log(error)
+                  this.loading=false
+              })
+            },
+            crear_curso:function () {
+              this.accion=1
+
+            },
+            cancelar_accion:function(){
+              this.accion=0
             }
           },
+          
           computed:{
             viewalertlist:function () {
               if(this.cursos.length==0){
