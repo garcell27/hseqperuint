2d287a1e5d1dfe761d416ae83ec42b970fdb9186
programador <garcell27>
2018-09-24 21:39:09 -0500

    24-09-2018

---
+++ src/views/portal/Banners.vue
@@ -0,0 +1,42 @@
+<template>
+   <div class="animated fadeIn">
+     <b-card>
+       <div slot="header" v-show="!loading">
+         <strong><i class="fa fa-list"></i> LISTA DE BANNERS</strong>
+         <div class="card-header-actions">
+           <b-link href="#" class="btn btn-success btn-sm" @click="crea_banner">
+             <i class="icon-plus"></i> Agregar
+           </b-link>
+         </div>
+       </div>
+       <b-row>
+         
+       </b-row>
+     </b-card>
+   </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import ElementLoading from 'vue-element-loading'
+    export default {
+        name: "Banners",
+        components:{
+            ElementLoading
+        },
+        data:function () {
+            return {
+              loading: false,
+            }
+        },
+        methods:{
+            crea_banner:function () {
+              
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
